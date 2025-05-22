642e0e25ebe426083457f91662230ac77518bed1
Mathewsq369 <mathewsq369@gmail.com>
2025-05-22 20:08:58 +0300

    added db

--- DIGIMANAGER/DIGIMANAGER/settings.py
+++ DIGIMANAGER/DIGIMANAGER/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'DIGIMANAGER.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DIGIMANAGER',
+        'USER' : 'root',
+        'PASSWORD' : 'p9ssword',
+        'HOST' : 'localhost',
+        'PORT' : '3306',
     }
 }
 
