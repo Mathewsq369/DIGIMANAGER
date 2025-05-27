2e611fcfcce9431ee44df9791f5968c17ecb3a8e
Mathewsq369 <mathewsq369@gmail.com>
2025-05-27 12:29:49 +0300

    DAY 1 - 5

---
+++ DIGIMANAGER/scheduler/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+
+
+# Create your models here.
+class Platform(models.Model):
+    name = models.CharField(max_length=50)
+    access_token = models.TextField()
+
+    def str(self):
+        return self.name
+
+class Post(models.Model):
+    STATUS_CHOICES = [
+        ('draft', 'Draft'),
+        ('scheduled', 'Scheduled'),
+        ('published', 'Published'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/', null=True, blank=True)
+    scheduled_time = models.DateTimeField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    def str(self):
+        return f"{self.user.username} - {self.platform.name} - {self.status}"
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('creator', 'Content Creator'),
+        ('manager', 'Manager'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
