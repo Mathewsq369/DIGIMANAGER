7ed8b1ee7f52ad96656ef6f43e4ce1500e083ad3
Mathewsq369 <mathewsq369@gmail.com>
2025-05-28 17:53:21 +0300

    correcting minor errors

--- DIGIMANAGER/scheduler/admin.py
+++ DIGIMANAGER/scheduler/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Platform, Post
 
 # Register your models here.
+admin.site.register(Platform)
+admin.site.register(Post)
--- DIGIMANAGER/scheduler/models.py
+++ DIGIMANAGER/scheduler/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
-
+from django.conf import settings
 
 # Create your models here.
 class Platform(models.Model):
@@ -17,7 +17,7 @@ class Post(models.Model):
         ('scheduled', 'Scheduled'),
         ('published', 'Published'),
     ]
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     platform = models.ForeignKey(Platform, on_delete=models.CASCADE)
     content = models.TextField()
     image = models.ImageField(upload_to='images/', null=True, blank=True)
